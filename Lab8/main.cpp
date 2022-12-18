00d39035cd1caab619aa1f3cd48b7a4bc9c7e282
N1fight <120026397+N1fight@users.noreply.github.com>
2022-12-18 16:03:20 +0300

    Lab8

---
+++ Lab8/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	unsigned long long a; //Занимает 8 байт
+	cout << "Enter number "; cin >> a; cout << endl;
+
+	unsigned char* p = (unsigned char*)&a; // unsigned char (диапозон значений от 0 до 255)
+
+
+	for (int i = 0; i < sizeof(unsigned long long); i++)
+	{
+		cout << "Byte " << i << ": " << (int)p[i] << endl;
+	}
+
+	return 0;
+}
